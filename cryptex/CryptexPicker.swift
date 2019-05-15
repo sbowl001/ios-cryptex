ed2359eef26e4774aa404fa625043dcce30f401d
Stephanie Bowles <sbowl001@fiu.edu>
2019-05-15 11:08:03 +0800

    trying to do code with the pickerView separate, not working

---
+++ cryptex/CryptexPicker.swift
@@ -0,0 +1,50 @@
+//
+//  CryptexPicker.swift
+//  cryptex
+//
+//  Created by Stephanie Bowles on 5/15/19.
+//  Copyr/Users/chichi/Documents/coding/ios-cryptex/cryptex/CryptexPicker.swiftight © 2019 Stephanie Bowles. All rights reserved.
+//
+
+import UIKit
+
+class CryptexPicker: UIPickerView, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    var cryptexController: CryptextController?
+//    var cryptex: Cryptex?
+    var letters = ["A", "B", "C", "D",
+                   "E", "F", "G", "H",
+                   "I", "J", "K", "L",
+                   "M", "N", "O", "P",
+                   "Q", "R", "S", "T",
+                   "U", "V", "W", "X",
+                   "Y", "Z"]
+
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        guard let cryptex = cryptexController?.currentCryptex else {return 0}
+        
+        return cryptex.password.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+       return letters.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        // For the title of each row, we want to show the letter that corresponds to the row. i.e. row 0 should show "A", row 1 should show "B", etc.
+         return letters[row]
+    }
+    
+    
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- cryptex/CryptexViewController.swift
+++ cryptex/CryptexViewController.swift
@@ -12,17 +12,30 @@ class CryptexViewController: UIViewController {
 
     @IBOutlet weak var label: UILabel!
     
-    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var pickerView: CryptexPicker!
     
     @IBOutlet weak var button: UIButton!
     
     
+    var cryptexController = CryptextController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+//        cryptexController.delegate = self
+//        pickerView.delegate = self
+//        pickerView.dataSource = self
+        updateViews()
         // Do any additional setup after loading the view.
     }
     
+    func updateViews() {
+//        Create an updateViews() method that takes the currentCryptex's hint and sets the hint label's text to it. It also should reload the components of the picker view. If we don't do this, when a new random cryptex is started, the picker view won't update to show the (potentially) new number of components (e.g. "Foo" has 3 components, but "Cryptex" has 7).
+        
+        guard let cryptex = cryptexController.currentCryptex else { return }
+        label.text = cryptex.hint
+        
+        pickerView.reloadAllComponents()
+    }
 
     @IBAction func buttonTapped(_ sender: Any) {
     }
