e4910b16141f5ac4d246a7942f8600270c921644
Stephanie Bowles <sbowl001@fiu.edu>
2019-05-15 09:46:40 +0800

    init setup

---
+++ cryptex/CryptexViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CryptexViewController.swift
+//  cryptex
+//
+//  Created by Stephanie Bowles on 5/15/19.
+//  Copyright © 2019 Stephanie Bowles. All rights reserved.
+//
+
+import UIKit
+
+class CryptexViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func buttonTapped(_ sender: Any) {
+    }
+    
+
+}
